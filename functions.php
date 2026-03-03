956dfae26f468303488ab59b4436828ba49acebc
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-03-03 10:45:28 +0100

    Ajout gestion fichier SVG (pour upload les images)

--- functions.php
+++ functions.php
@@ -71,3 +71,35 @@ function vh_enqueue_assets() {
     );
 }
 add_action('wp_enqueue_scripts', 'vh_enqueue_assets');
+
+//SVG Files
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+    global $wp_version;
+    if ( $wp_version !== '4.7.1' ) {
+       return $data;
+    }
+  
+    $filetype = wp_check_filetype( $filename, $mimes );
+  
+    return [
+        'ext'             => $filetype['ext'],
+        'type'            => $filetype['type'],
+        'proper_filename' => $data['proper_filename']
+    ];
+}, 10, 4 );
+  
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+  
+function fix_svg() {
+    echo '<style type="text/css">
+          .attachment-266x266, .thumbnail img {
+               width: 100% !important;
+               height: auto !important;
+          }
+          </style>';
+}
+  add_filter( 'upload_mimes', 'cc_mime_types' );
+  add_action( 'admin_head', 'fix_svg' );
